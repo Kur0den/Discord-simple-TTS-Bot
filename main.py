68bb36704cd044454193df842a9a2d708947d9a8
Kur0den0010 <69957747+Kur0den@users.noreply.github.com>
2023-12-17 23:14:15 +0900

    TTS保存用フォルダを作成する処理を追加

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 
+# ファイル操作関係
+import os
+import shutil
+
 # json関係
 import json
 
@@ -25,10 +29,24 @@ bot = commands.Bot(
 
 @bot.event
 async def on_ready():
+    # 起動時に実行する処理
+
+    # 設定ファイルから読み上げ用のフォルダを取得
+    bot.tts_folder = config["tts_folder"]
+
+    try:
+        shutil.rmtree(bot.tts_folder)
+    except FileNotFoundError:
+        pass
+    finally:
+        os.mkdir(bot.tts_folder)
     print("Bot is ready.")
 
 
-@app_commands.command(name="VCJoin", description="VCに参加して読み上げを開始します")
-@app_commands.guild_only(guild_ids=[config["guild_id"]])
-async def VC_join(interaction: discord.Interaction):
-    await interaction.response.send_message("VCに参加します")
+# @app_commands.command(name="VCJoin", description="VCに参加して読み上げを開始します")
+# @app_commands.guild_only(guild_ids=[config["guild_id"]])
+# async def VC_join(interaction: discord.Interaction):
+#     await interaction.response.send_message("VCに参加します")
+
+
+bot.run(config["token"])
